f28ccb05893c1a74c38b1eda5f1a10fa3818b760
lunaruiz715 <77206202+lunaruiz715@users.noreply.github.com>
2021-04-01 20:08:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function openSlideMenu(){
+  document.getElementById("side-menu").style.width="250px";
+}
+
+// function closeSlideMenu(){
+//   document.getElementById("side-menu").style.width="0";
+// }
+
+var i=document.querySelectorAll(".navLink").length;
+
+for(var n=0; n<i;n++){
+  var clickedlink=document.querySelectorAll(".navLink")[n];
+
+  clickedlink.addEventListener("click",function(){
+    document.getElementById("side-menu").style.width="0";
+  })
+
+}
+
+document.querySelector(".btn-close").addEventListener("click",function(){
+  document.getElementById("side-menu").style.width="0";
+})
